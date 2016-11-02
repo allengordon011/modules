37310e698ad665110ac30b89ad7bf69054f9d9d0
numbergames <bfletch@gmail.com>
2016-11-02 16:00:40 -0400

    make minor formatting changes

--- calcWithStartingValue.js
+++ calcWithStartingValue.js
@@ -1,26 +1,32 @@
  function calculator(startingValue) {
 	'use strict';
+
 	var _current = startingValue || 0;
 
 	function add(val) {
 		_current += val;
 		return this;
 	}
+
 	function subtract(val) {
 		_current -= val;
 		return this;
 	}
+
 	function multiply(val) {
 		_current *= val;
 		return this;
 	}
+
 	function divide(val) {
 		_current /= val;
 		return this;
 	}
+
 	function total() {
 		return _current;
 	}
+
 	function clear() {
 		_current = 0;
 		return _current;
@@ -38,4 +44,4 @@
 
 var myCalc = calculator(12);
 
-console.log(myCalc.multiply(5).add(3).total())
+console.log(myCalc.multiply(5).add(3).total());
